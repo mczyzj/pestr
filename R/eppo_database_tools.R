397f465ac86a57cd31db51dd0bc265e5b5256351
daredevilek <daredevilek@gmail.com>
2018-04-13 23:34:37 +0200

    updated tests, and updated eppo_names_table function

--- R/eppo_database_tools.R
+++ R/eppo_database_tools.R
@@ -56,38 +56,63 @@ eppo_database_connect <- function(filepath = getwd(),
 
 #' EPPO Data Services SQLite Database search tools
 #'
-#' \code{eppo_organism_check} checks if the names (or parts of the names) from
-#' vector are present in the database. \code{eppo_organism_prefnames} extracts
-#' preffered names of organisms from database. \code{eppo_organism_prefnames}
-#' extracts codeids from t_names table from EPPO RSQLite database.
+#' \code{eppo_names_table} checks if string (names or parts of the names)
+#' provided by user match any name in the EPPO database. If strings match any
+#' name in database, data frame with all names matching codeid, as well as
+#' preferred (binary), language and EPPOcode is returned.
 #'
-#' @param names A vector with organism names (or part of the names) to be
+#' @param names_vector A vector with organism names (or part of the names) to be
 #'    checked for existence in EPPO SQLite Database.
-#' @param sqlEppo connection to SQLite EPPO Database. By default NULL, function
-#' will automatically connect to database with default credentials.
+#' @param sqlConnection connection to SQLite EPPO Database. By default NULL;
+#'    function will automatically connect to database with default credentials.
 #' @return Check if provided names are present in EPPO SQLite Database, creates
-#'    data frame with preffered name and codeid.
+#'    list which contains: data frame with matching names in database and their
+#'    codeids, character vector of names from \code{names_vector} which do not
+#'    match any entry in database, data frame with preferred names and their
+#'    codeids, and data frame containing all names matching codeids of preffered
+#'    names. Last data frame contains also column with preferred (binary),
+#'    codeland (two letter character with language code), and EPPOcode.
 #' @name eppo_organism_names
 NULL
 
-eppo_organism_check <- function(names_vector, sqlConnection = NULL) {
+eppo_names_tables <- function(names_vector, sqlConnection = NULL) {
   #check if user provided db conection
   if (is.null(sqlConnection)) {
     sqlConnection <- eppo_database_connect()
   }
   #extract entries from SQLite db that match names in names_vector
   names_in_DB <- sqlConnection %>%
-    DBI::dbGetQuery(paste0('SELECT codeid, fullname, preferred FROM t_names
+    DBI::dbGetQuery(paste0('SELECT codeid, fullname FROM t_names
                            WHERE fullname LIKE ',
                            paste(paste0("'%", names_vector, "%'"),
                                  collapse = " OR fullname LIKE ")))
   #intermediet list with that checks if element of names_vector
   #match element in SQLite db
   test_list <- lapply(names_vector, grep, names_in_DB$fullname)
+  #extracts EPPO codes for unique codeid
+  EPPOcodes <- sqlConnection %>%
+    DBI::dbGetQuery(paste0('SELECT codeid, eppocode FROM t_codes
+                           WHERE codeid IN (',
+                           paste0(unique(names_in_DB$codeid), collapse = ', '),
+                           ')'))
+  #creates data frame with all names that match codeid from names_in_DB
+  all_names <- sqlConnection %>%
+    DBI::dbGetQuery(paste0('SELECT codeid, fullname, preferred, codelang, status
+                           FROM t_names WHERE codeid IN (',
+                           paste(unique(names_in_DB$codeid), collapse = ', '),
+                           ')')) %>%
+    filter(status == 'A')
 
-  return(list(exist_in_DB = data.frame(names_in_DB),
-       not_in_DB   = names_vector[lapply(names_vector,
-                                         grep,
-                                         names_in_DB$fullname) %>%
-                                    sapply(length) == 0]))
+
+  return(list(exist_in_DB     = data.frame(names_in_DB),
+              not_in_DB       = names_vector[test_list %>%
+                                               sapply(length) == 0],
+              pref_names = data.frame(all_names %>%
+                                        filter(preferred == 1) %>%
+                                        select(codeid, fullname) %>%
+                                        left_join(EPPOcodes, by = 'codeid')),
+              all_associated_names  = data.frame(all_names %>%
+                                                   select(-status) %>%
+                                                   left_join(EPPOcodes,
+                                                             by = 'codeid'))))
 }
--- tests/testthat/test_dbnameextraction.R
+++ tests/testthat/test_dbnameextraction.R
@@ -3,20 +3,38 @@ library(pestr)
 
 test_that("Test that f returns correct names from database", {
   testNames <- c('Cydia packardi', 'noPest', 'cherry virus', 'noPest2')
-  resultNames <- list(exist_in_DB = c("Cydia packardi",
-                                      "Cherry little cherry virus",
-                                      "Epirus cherry virus",
-                                      "Cherry little cherry virus",
-                                      "Cherry virus A",
-                                      "Little cherry virus 1",
-                                      "Little cherry virus 2"),
+  resultNames <- list(exist_in_DB = c('Cydia packardi',
+                                      'Cherry little cherry virus',
+                                      'Epirus cherry virus',
+                                      'Cherry little cherry virus',
+                                      'Cherry virus A',
+                                      'Little cherry virus 1',
+                                      'Little cherry virus 2'),
                       not_in_DB = c('noPest', 'noPest2'))
-  expect_is(DBI::dbGetQuery(RSQLite::dbConnect(RSQLite::SQLite(),
-                                               dbname = 'eppocodes.sqlite'),
-                            "SELECT fullname FROM t_names WHERE fullname LIKE 'Cydia packardi'"), 'data.frame')
-  expect_is(eppo_organism_check(testNames), 'list')
-  expect_is(eppo_organism_check(testNames)$exist_in_DB, 'data.frame')
-  expect_is(eppo_organism_check(testNames)$not_in_DB, 'character')
-  expect_equal(eppo_organism_check(testNames)[[1]]$fullname, resultNames[[1]])
-  expect_equal(eppo_organism_check(testNames)[[2]], resultNames[[2]])
+  preferredNames <- c('Grapholita packardi',
+                      'Epirus cherry virus',
+                      'Cherry virus A',
+                      'Little cherry virus 1',
+                      'Little cherry virus 2')
+
+  expect_equal(eppo_names_tables(testNames)[[1]]$fullname, resultNames[[1]])
+  expect_equal(eppo_names_tables(testNames)[[2]], resultNames[[2]])
+  expect_equal(eppo_names_tables(testNames)[[3]]$fullname, preferredNames)
 })
+
+test_that("Test that f returns empty tables and non empty vector of
+          incorect names when all names in query are invalid", {
+
+  expect_is(eppo_names_tables('nonSenS12'), 'list')
+  expect_is(eppo_names_tables('nonSenS12')$exist_in_DB, 'data.frame')
+  expect_is(eppo_names_tables('nonSenS12')$not_in_DB, 'character')
+  expect_is(eppo_names_tables('nonSenS12')$pref_names, 'data.frame')
+  expect_is(eppo_names_tables('nonSenS12')$all_associated_names, 'data.frame')
+
+  expect_equal(dim(eppo_names_tables('nonSenS12')[[1]]), c(0L, 2L))
+  expect_equal(eppo_names_tables('nonSenS12')[[2]], 'nonSenS12')
+  expect_equal(dim(eppo_names_tables('nonSenS12')[[3]]), c(0L, 3L))
+  expect_equal(dim(eppo_names_tables('nonSenS12')[[4]]), c(0L, 5L))
+})
+
+# make S3 class, define print and summary methods
