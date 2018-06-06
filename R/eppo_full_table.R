3f3e5acd42aca390731090c6ebc3dc4853fd8968
mczyzj <mczyzj@gmail.com>
2018-06-06 11:52:29 +0200

    eppo_full_table function to join all the compact tables by eppocode column

---
+++ R/eppo_full_table.R
@@ -0,0 +1,40 @@
+#' EPPO Data Services information table
+#'
+#' \code{eppo_table_full} creates table in human friendly format with data on
+#' pest preferred name, synonyms and common names, hosts, categorization,
+#' taxonomy and its distribution.
+#'
+#' @param names_vector A vector with organism names (or part of the names) to be
+#'    checked for existence in EPPO SQLite Database.
+#' @param sqlConnection connection to SQLite EPPO Database. By default NULL;
+#'    function will automatically connect to database with default credentials.
+#' @param token An object containing EPPO API token created via
+#'   {\link{create_eppo_token}}.
+#' @return Wraps results of function from \code{eppo_tabletools}, namely:
+#'    {\link{eppo_tabletools_names}}, {\link{eppo_tabletools_hosts}},
+#'    {\link{eppo_tabletools_cat}}, {\link{eppo_tabletools_taxo}}, and
+#'    {\link{eppo_tabletools_distri}} into one compact, human readable table.
+#' @export
+eppo_table_full <- function(names_vector, sqlConnection, token) {
+  if (!all(class(token) == c('pestr_token', 'character'))) {
+    message('Your token argument is not of pestr_token class.
+            Please provide token created with create_eppo_token function')
+  } else {
+
+  names_tables <- eppo_names_tables(names_vector, sqlConnnection)
+
+  compact_names  <- eppo_tabletools_names(names_tables)
+  compact_hosts  <- eppo_tabletools_hosts(names_tables, token)
+  compact_cat    <- eppo_tabletools_cat(names_tables, token)
+  compact_taxo   <- eppo_tabletools_taxo(names_tables, token)
+  compact_distri <- eppo_tabletools_distri(names_tables)
+
+  full_table  <-  compact_names[[2]] %>%
+    dplyr::full_join(compact_hosts[[2]], by = 'eppocode') %>%
+    dplyr::full_join(compact_cat[[2]], by = 'eppocode') %>%
+    dplyr::full_join(compact_distri[[2]], by = 'eppocode') %>%
+    dplyr::full_join(compact_taxo[[2]], by = 'eppocode')
+
+  return(full_table)
+  }
+}
