c3896ed96bf28e19e1753de9436480b0ac6d24a9
mczyzj <m.czyz.j@gmail.com>
2020-04-12 14:37:07 +0200

    helper function to download distribution csv

--- R/eppo_api.R
+++ R/eppo_api.R
@@ -28,3 +28,25 @@ eppo_rest_download <- function(eppocodes, type, token) {
 eppo_json_wrapper <- function(urls) {
   jsonlite::fromJSON(httr::content(httr::GET(urls), "text"))
 }
+
+#' EPPO distribution helper tools
+#'
+#' Set of small wrappers and functions to help connecting with API and reuse
+#' among other package functions
+#'
+#' @param eppocodes character vector of eppocodes to be downloaded.
+#' @return List of CSV download results.
+#' @noRd
+
+eppo_csv_download <- function(eppocodes) {
+  distri_urls <- paste0('https://gd.eppo.int/taxon/',
+                        eppocodes,'/download/distribution_csv')
+
+  distri_lists <- stats::setNames(vector("list", length(eppocodes)), eppocodes)
+
+  for (i in 1:length(distri_lists)) {
+    distri_lists[i][[1]] <- utils::read.csv(file = distri_urls[i],
+                                            header = T, stringsAsFactors = F)
+  }
+  return(distri_lists)
+}
--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -1,6 +1,7 @@
 context("EPPO API helpers")
 library(pestr)
 
+#test if eppo_rest_download is downloading resuts into list
 test_that('Test that eppo_rest_download is downloading correctly', {
   skip_on_travis()
   skip_on_cran()
@@ -8,6 +9,16 @@ test_that('Test that eppo_rest_download is downloading correctly', {
   create_eppo_token('')
   eppocode_1 <- "XYLEFA"
   expect_true(is.list(eppo_rest_download(eppocode_1, "hosts", eppo_token)))
+})
+
+#tes if eppo_csv_download is downloading results into list
+
+test_that('Test that eppo_csc_download is downloading correctly' , {
+  skip_on_travis()
+  skip_on_cran()
+  skip("Only for local use due to downloading files") #comment out to test
+  eppocode_1 <- "XYLEFA"
+  expect_true(is.list(eppo_csv_download(eppocode_1)))
 })
 
 # rm(eppo_token, envir = globalenv())
