02de25439cc261b5a3a336738efbeeeeb6e784ce
Michal <m.czyz.j@gmail.com>
2020-12-31 14:31:45 +0100

    eppo_try_urls test

--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -11,14 +11,28 @@ test_that('Test that eppo_rest_download is downloading correctly', {
   expect_true(is.list(eppo_rest_download(eppocode_1, "hosts", eppo_token)))
 })
 
-#tes if eppo_csv_download is downloading results into list
+#test if eppo_csv_download is downloading results into list
 
-test_that('Test that eppo_csc_download is downloading correctly' , {
+test_that('Test that eppo_csc_download is downloading correctly', {
   skip_on_travis()
   skip_on_cran()
   skip("Only for local use due to downloading files") #comment out to test
   eppocode_1 <- "XYLEFA"
   expect_true(is.list(eppo_csv_download(eppocode_1)))
 })
+
+#test if eppo_try_urls returns error when there is no token
+
+test_that('Test that eppo_try_urls returns error', {
+  skip_on_travis()
+  skip_on_cran()
+  create_eppo_token('')
+  eppocode_1 <- "XYLEFA"
+  test_link <- paste0('https://data.eppo.int/api/rest/1.0/taxon/',
+                      "XYLEFA", "/hosts", eppo_token)
+  expect_message(eppo_try_urls(test_link))
+}
+
+)
 
 # rm(eppo_token, envir = globalenv())
